28327074b948c7048d5485a68249f4cfa36b8143
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-20 10:40:10 -0300

    feat: Create 'USER' table migration

---
+++ typeorm/migration/1687267925756-Migrate.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+
+export class Migrate1687265506002 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'USER',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    unsigned: true,
+                    isGenerated: true,
+                    isPrimary: true,
+                    generationStrategy: 'increment',
+                },
+
+                {
+                    name: 'name',
+                    type: 'varchar',
+                    length: '63',
+                },
+
+                {
+                    name: 'email',
+                    type: 'varchar',
+                    length: '127',
+                    isUnique: true
+                },
+
+                {
+                    name: 'password',
+                    type: 'varchar',
+                    length: '127',
+                },
+
+                {
+                    name: 'birthAt',
+                    type: 'date',
+                    isNullable: true
+                },
+                {
+                    name: 'createdAt',
+                    type: 'timestamp',
+                    precision: 6,
+                    default: 'CURRENT_TIMESTAMP()',
+                },
+                {
+                    name: 'updatedAt',
+                    type: 'timestamp',
+                    precision: 6,
+                    default: 'CURRENT_TIMESTAMP()',
+                },
+            ],
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('USER');
+    }
+}
