a76903dad7f1b6dba190806840f5b3c94d273fad
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-20 10:25:45 -0300

    feat: Update AppModule with configuration module and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { UserEntity } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
-      username: "root",
-      password: 'admin',
-      database: "API",
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [UserEntity],
-      synchronize: true
+      synchronize: process.env.NODE_ENV === "development"
     })
   ],
   controllers: [AppController],
