70304f4a41940c10133d5132f71c3cade526b5c7
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-20 07:33:52 -0300

    refactor: Update UserModule to use TypeORM repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [],
   exports: [],
