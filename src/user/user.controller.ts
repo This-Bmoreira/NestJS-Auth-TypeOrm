01bb12e5627842ee0204b1bfea29bb7636241e15
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-20 08:20:15 -0300

    Updated:  create method to call the userService.create method instead of returning the body .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } 
 import { CreateUserDTO } from "./DTO/create-user.dto";
 import { UpdatePatchUserDTO } from "./DTO/update-patch-user.dto";
 import { UpdatePutUserDTO } from "./DTO/update-put-user.dto";
+import { UserService } from "./user.service";
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) { }
   @Post()
   async create(@Body() body: CreateUserDTO) {
-    return { body }
+    return this.userService.create(body)
   }
   @Get()
   async read() {
