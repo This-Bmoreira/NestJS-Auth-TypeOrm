d5869c7baeb0c9fafe9e12a596a28207dbc1ce71
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-22 09:49:01 -0300

    Updated: AuthModule with UserModule integration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from '../user/user.module';
 @Module({
   imports: [
     JwtModule.register({
       secret: String(process.env.JWT_SECRET)
     }),
+    UserModule
   ],
 })
 
