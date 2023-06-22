00306e49320430a07c33d38dcf6ba0a08d7d6dc7
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-22 10:09:26 -0300

    Updated: AuthModule with UserEntity TypeOrmModule integration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entity/user.entity';
 @Module({
   imports: [
     JwtModule.register({
       secret: String(process.env.JWT_SECRET)
     }),
-    UserModule
+    UserModule,
+    TypeOrmModule.forFeature([UserEntity])
   ],
 })
 
