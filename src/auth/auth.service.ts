a13d6b893272f5bd513797d257a945202d4dcc40
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-22 10:08:27 -0300

    Updated: AuthService with UserRepository injection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../user/entity/user.entity';
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) { }
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>
+  ) { }
   async createToken() {
     // return this.jwtService.sign()
   }
