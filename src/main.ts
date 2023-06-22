616f1c27695076a38d751ad43480da353cce1754
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-22 08:07:16 -0300

    feat: add LogInterceptor to global interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LogInterceptor } from './user/interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LogInterceptor())
   app.useGlobalPipes(new ValidationPipe())
   const openApiDocument = require('../openapi.json');
   SwaggerModule.setup('api', app, openApiDocument);
