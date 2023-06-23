5653378022232ca6406a3cbc67fde9201b8aa48f
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-23 10:58:50 -0300

    feat: Implement RoleGuard canActivate method

---
+++ src/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, Injectable, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/role.decorator';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) { }
+
+  async canActivate(context: ExecutionContext) {
+    const requeridRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    console.log({requeridRoles})
+   
+    if (!requeridRoles) {
+      return true;
+    } 
+     const { user } = context.switchToHttp().getRequest();
+    console.log({ requeridRoles, user })
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,35 +1,44 @@
-import { Body, Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Patch, Post, Put, UseGuards } from "@nestjs/common";
+import { ParamId } from "../decorators/param-id.decorator";
+import { Roles } from "../decorators/role.decorator";
+import { Role } from "../enums/role.enum";
+import { RoleGuard } from "../guards/role.guard";
 import { CreateUserDTO } from "./DTO/create-user.dto";
 import { UpdatePatchUserDTO } from "./DTO/update-patch-user.dto";
 import { UpdatePutUserDTO } from "./DTO/update-put-user.dto";
 import { UserService } from "./user.service";
-import { ParamId } from "../decorators/param-id.decorator";
 
+@UseGuards(RoleGuard)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) { }
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() body: CreateUserDTO) {
     return this.userService.create(body)
   }
+  @Roles(Role.Admin, Role.User)
   @Get()
   async list() {
     return this.userService.list()
   }
+  @Roles(Role.Admin, Role.User)
   @Get(':id')
   async show(@ParamId() id: number) {
     return this.userService.show(id)
   }
+  @Roles(Role.Admin)
   @Put(':id')
   async update(@Body() body: UpdatePutUserDTO, @ParamId() id: number) {
     return this.userService.update(id, body)
   }
+  @Roles(Role.Admin)
   @Patch(':id')
   async updatePartial(@Body() body: UpdatePatchUserDTO, @ParamId() id: number) {
     return this.userService.updatePartial(id, body)
 
   }
-
+  @Roles(Role.Admin)
   @Delete(':id')
   async delete(@ParamId() id: number) {
     return this.userService.delete(id)
