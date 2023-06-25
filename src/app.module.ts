e61b2f4669dbf0bfd1038a0858142e0cb499868f
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-25 09:21:58 -0300

    feat: Add email functionality and templates to AppModule configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserEntity } from './user/entity/user.entity';
 import { UserModule } from './user/user.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -26,7 +28,28 @@ import { UserModule } from './user/user.module';
       database: process.env.DB_DATABASE,
       entities: [UserEntity],
       synchronize: process.env.NODE_ENV === "development"
-    })
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'rowena.kessler77@ethereal.email',
+          pass: 'eaN42qSUZA5ATs7KRA'
+        }
+      },
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+
+      template: {
+        dir: join(__dirname, '..', 'templates'),
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
