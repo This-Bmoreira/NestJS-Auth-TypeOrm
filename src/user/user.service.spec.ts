23a618596b40cd8b1ef0afe82f7094451c919ab8
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-26 08:58:57 -0300

    feat: Add initial userEntityList data

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { createUserDTO } from "../testing/create-use-dto.mock";
 import { userRepositoryMock } from "../testing/use-repository.mock";
 import { UserService } from "./user.service";
+import { userEntityList } from "../testing/user-entity-list.mocks";
 
 describe('UserService', () => {
   let userService: UserService;
@@ -16,5 +18,12 @@ describe('UserService', () => {
 
   test('Validar a definição ', () => {
     expect(userService).toBeDefined()
+  })
+
+  describe('create', () => {
+    test('method create', async () => {
+      const result = await userService.create(createUserDTO)
+      expect(result).toEqual(userEntityList[0])
+    })
   })
 })
