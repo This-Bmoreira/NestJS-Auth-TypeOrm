9765afb1ae5af8104a6808c9251b6dd0331ef63a
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-26 09:00:00 -0300

    Revert "feat: Add initial userEntityList data"
    This reverts commit 23a618596b40cd8b1ef0afe82f7094451c919ab8.

--- src/testing/use-repository.mock.ts
+++ src/testing/use-repository.mock.ts
@@ -1,12 +1,11 @@
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { UserEntity } from "../user/entity/user.entity";
-import { userEntityList } from "./user-entity-list.mocks";
 
 export const userRepositoryMock = {
   provide: getRepositoryToken(UserEntity),
   useValue: {
     create: jest.fn(),
-    save: jest.fn().mockResolvedValue(userEntityList[0]),
+    save: jest.fn(),
     find: jest.fn(),
     findOneBy: jest.fn(),
     update: jest.fn(),
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,8 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { createUserDTO } from "../testing/create-use-dto.mock";
 import { userRepositoryMock } from "../testing/use-repository.mock";
 import { UserService } from "./user.service";
-import { userEntityList } from "../testing/user-entity-list.mocks";
 
 describe('UserService', () => {
   let userService: UserService;
@@ -18,12 +16,5 @@ describe('UserService', () => {
 
   test('Validar a definição ', () => {
     expect(userService).toBeDefined()
-  })
-
-  describe('create', () => {
-    test('method create', async () => {
-      const result = await userService.create(createUserDTO)
-      expect(result).toEqual(userEntityList[0])
-    })
   })
 })
