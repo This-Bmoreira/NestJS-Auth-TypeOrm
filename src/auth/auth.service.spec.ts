24cc7386bca7ea833745f5c4dec097d0f2c9c0b3
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-27 10:23:50 -0300

    test: add token-related tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { accessToken } from "../testing/access-token.mock";
+import { jwtPayload } from "../testing/jwt-payload.mock";
 import { jwtServiceMock } from "../testing/jwt-service.mock";
 import { mailerServiceMock } from "../testing/mailer-service.mock";
 import { userRepositoryMock } from "../testing/use-repository.mock";
 import { userServiceMock } from "../testing/user-service.mock";
+import { userEntityList } from "../testing/user.entity-list.mocks";
 import { AuthService } from "./auth.service";
 
 describe('AuthService', () => {
@@ -25,4 +28,25 @@ describe('AuthService', () => {
   test('Validar a definição', () => {
     expect(authService).toBeDefined();
   });
+
+  describe('token', () => {
+    test('createToken method', () => {
+      const result = authService.createToken(userEntityList[0]);
+      expect(result).toEqual({
+        accessToken
+      })
+    })
+    test('checkToken method', () => {
+      const result = authService.checkToken(accessToken);
+      expect(result).toEqual({
+        jwtPayload
+      })
+    })
+    test('isValidToken method', () => {
+      const result = authService.isValidToken(accessToken);
+      expect(result).toEqual(true)
+    })
+  })
+
+
 })
