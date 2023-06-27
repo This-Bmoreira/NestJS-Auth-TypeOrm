987f60321e797f18dcc476d32b1a8f21088b145f
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-06-27 16:59:40 -0300

    feat: userServiceMock with resolved values in mock functions

--- src/testing/user-service.mock.ts
+++ src/testing/user-service.mock.ts
@@ -1,14 +1,15 @@
 import { UserService } from "../user/user.service";
+import { userEntityList } from "./user.entity-list.mocks";
 
 export const userServiceMock = {
   provide: UserService,
   useValue: {
-    show: jest.fn(),
-    create: jest.fn(),
-    list: jest.fn(),
-    update: jest.fn(),
-    updatePartial: jest.fn(),
-    delete: jest.fn(),
-    exists: jest.fn(),
+    show: jest.fn().mockResolvedValue(userEntityList[0]),
+    create: jest.fn().mockResolvedValue(userEntityList[0]),
+    list: jest.fn().mockResolvedValue(userEntityList),
+    update: jest.fn().mockResolvedValue(userEntityList[0]),
+    updatePartial: jest.fn().mockResolvedValue(userEntityList[0]),
+    delete: jest.fn().mockResolvedValue(true),
+    exists: jest.fn().mockResolvedValue(true),
   }
 }
